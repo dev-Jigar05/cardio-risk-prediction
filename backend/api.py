00d03c8aca9ce675f7d210be3cec8bc3f25feb9f
Jigar <jigarvadukar03@gmail.com>
2025-12-16 11:07:29 +0530

    api.py changed

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+import os
 
 app = FastAPI(title="Cardio Risk API")
 
-# lr_model = joblib.load("cardio_model_lr.pkl")
-# rf_model = joblib.load("cardio_model_rf.pkl")
-knn_model = joblib.load("cardio_model_knn.pkl")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://cardio-risk-prediction.streamlit.app"
+        "localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "cardio_model_knn.pkl")
+knn_model = joblib.load(model_path)
+
 
 class CardioInput(BaseModel):
     age: int
@@ -23,43 +37,23 @@ class CardioInput(BaseModel):
     active: int
 
 
+@app.get("/")
+def health():
+    return {"status": "API running"}
+
+
 @app.post("/predict")
-def predict_risk(
-    data: CardioInput,
-    model_name: str = "lr" 
-):
-    
-    # if model_name == "rf":
-    #     used_model = rf_model
-    # else:
-    #     used_model = lr_model
-
-    used_model = knn_model
-
-    d = dict(data)
-
+def predict_risk(data: CardioInput):
+    d = data.dict()
     bmi = d["weight"] / ((d["height"] / 100) ** 2)
 
-    input_df = pd.DataFrame([{
-        "age": d["age"],
-        "gender": d["gender"],
-        "height": d["height"],
-        "weight": d["weight"],
-        "ap_hi": d["ap_hi"],
-        "ap_lo": d["ap_lo"],
-        "cholesterol": d["cholesterol"],
-        "gluc": d["gluc"],
-        "smoke": d["smoke"],
-        "alco": d["alco"],
-        "active": d["active"],
-        "bmi": bmi
-    }])
+    input_df = pd.DataFrame([{**d, "bmi": bmi}])
 
-    proba = float(used_model.predict_proba(input_df)[0][1])
-    pred = int(used_model.predict(input_df)[0])
+    proba = float(knn_model.predict_proba(input_df)[0][1])
+    pred = int(knn_model.predict(input_df)[0])
 
     return {
-        "model_used": "Random Forest" if model_name == "rf" else "Logistic Regression",
+        "model_used": "KNN",
         "prediction": pred,
         "risk_label": "High" if pred == 1 else "Low",
         "probability": proba
