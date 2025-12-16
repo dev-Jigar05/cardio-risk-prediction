3ece165f4ce85c1f3f8d36a26f56eabbb1113c17
Jigar <jigarvadukar03@gmail.com>
2025-12-16 11:14:05 +0530

    changed api url from loacalhost to onrender

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import requests
 
 st.set_page_config(page_title="Cardio Risk Predoctor", page_icon="🫀")
 
-API_URL = "http://127.0.0.1:8000/predict"  
+API_URL = "https://cardio-risk-prediction-1.onrender.com"
 
 st.title("Cardio Disease Prediction")
 st.write("Enter the details below to estimate cardiovascular risk.")
